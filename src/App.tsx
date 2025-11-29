afbdca7d554511e56dae1c37099b28db614bd246
saurabhumai <160607509+saurabhumai@users.noreply.github.com>
2025-11-29 15:26:49 +0530

    Fix BrowserRouter basename for GitHub Pages deployment

--- src/App.tsx
+++ src/App.tsx
@@ -13,8 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
-        <Routes>
+      <BrowserRouter basename="/saurabh-s-cloud-portfolio">        <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />

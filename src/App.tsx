0cc0c06c7d9bac4ba3fb1869f0fbaf0419c72753
saurabhumai <160607509+saurabhumai@users.noreply.github.com>
2025-11-29 15:29:47 +0530

    Replace BrowserRouter with HashRouter for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
+import { HashRouter, Routes, Route } from "react-router-dom";import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -13,13 +12,11 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/saurabh-s-cloud-portfolio">        <Routes>
-          <Route path="/" element={<Index />} />
+      <HashRouter>          <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
+      </HashRouter>    </TooltipProvider>
   </QueryClientProvider>
 );
 
